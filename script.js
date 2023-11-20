da42bc1b626e1d121232b2def906c2dc0bdba0c7
Leonardo De Oliveira <leonardodeoliveira@outlook.com>
2023-11-20 15:02:25 -0300

    comitando projeto finalizado

---
+++ script.js
@@ -0,0 +1,30 @@
+let chave = "cebcd482eda57fa9a6714c1c2ba91885"
+
+function colocarNaTela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = " " + dados.name
+    document.querySelector(".pais").innerHTML = " " + dados.sys.country
+    document.querySelector(".max").innerHTML = "Max: " + Math.floor(dados.main.temp_max) + "°C"
+    document.querySelector(".min").innerHTML = "Min: " + Math.floor(dados.main.temp_min) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + " %" 
+}
+
+async function buscarCidade(cidade) {
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+    cidade +
+    "&appid=" +
+    chave +
+    "&lang=pt-br" +
+    "&units=metric"
+    ).then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    let cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
